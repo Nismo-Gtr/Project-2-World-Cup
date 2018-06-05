b6f5972b889caebd883671fdaaf040cd7b78b97f
Sadeq Rahman <sadeq@dhcp-10-102-143-15.wireless.northwestern.private>
2018-06-05 14:09:01 -0500

    added custom navbar buttons and modals. OAUTH to be added next.

--- public/login.js
+++ public/login.js
@@ -15,93 +15,98 @@ var firebase_config = {
 firebase.initializeApp(firebase_config);
 var database = firebase.database();
 
-// User profile that is loaded
-var currentUser;
 
-$("#loginButton").on("click", function () {
-    event.preventDefault();
+$(document).ready(function () {
 
-    // Capturing user email and password and saving to local temporary object
-    var userCredentials = {
-        email: $("#userID").val().trim(),
-        password: $("#password").val().trim()
-    }
+    // User profile that is loaded
+    var currentUser;
 
-    console.log("userCredentials", userCredentials);
+    $("#loginButton").on("click", function () {
+        event.preventDefault();
 
-    // This whole things works even without a post request. Is a post request necessary? 
+        // Capturing user email and password and saving to local temporary object
+        var userCredentials = {
+            email: $("#loginID").val().trim(),
+            password: $("#loginPassword").val().trim()
+        }
 
-    // Send the POST request.
-    // if (userCredentials.email && userCredentials.password) {
-    $.ajax("/authenticate", {
-        type: "POST",
-        data: userCredentials
-    }).then(function (response) {
-        console.log("About to check user credentials in firebase now");
-        // Reload the page to get the updated list
-        // location.reload();
-        // console.log("POST response: ", response);
-        // console.log("response.email: ", response.email);
-        // console.log("response.password: ", response.password);
-        
-        // check the database for matching email/password pairs
-        database.ref().on("value", function (snapshot) {
-            data = snapshot.val();
-            // console.log("DATA: ", data);
-            // console.log("==========================");
-            var keys = Object.keys(data);
-            for (var i = 0; i < keys.length; i++) {
-                // console.log("data[keys[i]].email", data[keys[i]].email);
-                if (response.email === data[keys[i]].email && response.password === data[keys[i]].password) {
-                    console.log("username and password combinations EXISTS");
-                    currentUser = [keys[i]];
-                    console.log(currentUser);
-                    // add username for each user. then you can access currentUser.username
-                } 
-            }
+        console.log("userCredentials", userCredentials);
+
+        // This whole things works even without a post request. Is a post request necessary? 
+
+        // Send the POST request.
+        // if (userCredentials.email && userCredentials.password) {
+        $.ajax("/authenticate", {
+            type: "POST",
+            data: userCredentials
+        }).then(function (response) {
+            console.log("About to check user credentials in firebase now");
+            // Reload the page to get the updated list
+            // location.reload();
+            // console.log("POST response: ", response);
+            // console.log("response.email: ", response.email);
+            // console.log("response.password: ", response.password);
+
+            // check the database for matching email/password pairs
+            database.ref().on("value", function (snapshot) {
+                data = snapshot.val();
+                // console.log("DATA: ", data);
+                // console.log("==========================");
+                var keys = Object.keys(data);
+                for (var i = 0; i < keys.length; i++) {
+                    // console.log("data[keys[i]].email", data[keys[i]].email);
+                    if (response.email === data[keys[i]].email && response.password === data[keys[i]].password) {
+                        console.log("username and password combinations EXISTS");
+                        currentUser = [keys[i]];
+                        console.log(currentUser);
+                        // add username for each user. then you can access currentUser.username
+                    }
+                }
+            });
+
         });
 
-    });
 
-
-    // }
-
-});
-
-// signup page
-
-$("#signupButton").on("click", function () {
-    event.preventDefault();
-
-    // Capturing user email and password and saving to local temporary object
-    var userCredentials = {
-        username: $("#username").val().trim(),
-        email: $("#userID").val().trim(),
-        password: $("#password").val().trim()
-    }
-
-    console.log("userCredentials", userCredentials);
-
-    // Send the POST request.
-    // if (userCredentials.email && userCredentials.password) {
-    $.ajax("/authenticate", {
-        type: "POST",
-        data: userCredentials
-    }).then(function (response) {
-        console.log("About to check user credentials in firebase now");
-        // Reload the page to get the updated list
-        // location.reload();
-        console.log("POST response: ", response);
-        console.log("response.username", response.username);
-        console.log("response.email: ", response.email);
-        console.log("response.password: ", response.password);
-        database.ref().push(response);
+        // }
 
     });
 
+    // signup page
 
-    // }
+    $("#signupButton").on("click", function () {
+        event.preventDefault();
+
+        // Capturing user email and password and saving to local temporary object
+        var userCredentials = {
+            username: $("#userName").val().trim(),
+            email: $("#userEmail").val().trim(),
+            password: $("#userPassword").val().trim()
+        }
+
+        console.log("userCredentials", userCredentials);
+
+        // Send the POST request.
+        // if (userCredentials.email && userCredentials.password) {
+        $.ajax("/authenticate", {
+            type: "POST",
+            data: userCredentials
+        }).then(function (response) {
+            console.log("About to check user credentials in firebase now");
+            // Reload the page to get the updated list
+            // location.reload();
+            console.log("POST response: ", response);
+            console.log("response.username", response.username);
+            console.log("response.email: ", response.email);
+            console.log("response.password: ", response.password);
+            database.ref().push(response);
+
+        });
+
+
+        // }
+
+    });
+
+    // oauth2 page for facebook, twitter and google.
 
 });
-
-// oauth2 page for facebook, twitter and google.

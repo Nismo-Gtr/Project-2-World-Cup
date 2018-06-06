5fbdbbd57cba1b5c0d38e34216eef48731e13448
m0bius <ezra.cook@u.northwestern.edu>
2018-06-06 14:18:17 -0500

    merge for lat lng

--- schema.sql
+++ schema.sql
@@ -17,7 +17,6 @@ CREATE TABLE `stadiums` (
   `lat`  FLOAT ( 11 ) NOT NULL,
   `lng` FLOAT ( 11 ) NOT NULL,
   `image` VARCHAR ( 255 ) NOT NULL,
-  `created_at` DATETIME NOT NULL,
 
   PRIMARY KEY ( `id` ) 
 );
@@ -29,7 +28,6 @@ CREATE TABLE `brackets` (
   `stadium` VARCHAR ( 255 ) NOT NULL,
   `channels` VARCHAR ( 255 ) NOT NULL,
   `matchday` VARCHAR ( 255 ) NOT NULL,
-  `created_at` DATETIME NOT NULL,
 
   PRIMARY KEY ( `id` ) 
 );
--- seeds.sql
+++ seeds.sql
@@ -33,6 +33,17 @@ INSERT INTO teams (name, emoji, emojiString) VALUES ("Colombiua", "flag-co", "�
 INSERT INTO teams (name, emoji, emojiString) VALUES ("Japan", "flag-jp", "🇯🇵");
 
 
-INSERT INTO stadiums (name, city, lat, lng, image) VALUES ("name", "city", "lat", "lng", "image");
+INSERT INTO stadiums (name, city, lat, lng, image) VALUES ("Luzhniki Stadium", "Moscow", 55.715765, 37.5515217, "https://upload.wikimedia.org/wikipedia/commons/e/e6/Luzhniki_Stadium%2C_Moscow.jpg");
+INSERT INTO stadiums (name, city, lat, lng, image) VALUES ("Otkrytiye Arena", "Moscow", 55.817765, 37.440363, "https://upload.wikimedia.org/wikipedia/commons/5/50/Stadium_Spartak_in_Moscow.jpg");
+INSERT INTO stadiums (name, city, lat, lng, image) VALUES ("Krestovsky Stadium", "Saint Petersburg", 59.97274, 30.221408, "https://upload.wikimedia.org/wikipedia/commons/a/ad/Spb_06-2017_img40_Krestovsky_Stadium.jpg");
+INSERT INTO stadiums (name, city, lat, lng, image) VALUES ("Kaliningrad Stadium", "Kazan", 55.820983, 49.160966, "https://upload.wikimedia.org/wikipedia/commons/a/a4/Kaliningrad_stadium_-_2018-04-07.jpg");
+INSERT INTO stadiums (name, city, lat, lng, image) VALUES ("Kazan Arena", "Kazan", 55.820983, 49.160966, "https://upload.wikimedia.org/wikipedia/commons/3/3a/Kazan_Arena_08-2016.jpg");
+INSERT INTO stadiums (name, city, lat, lng, image) VALUES ("Nizhny Novogorod Stadium", "Nizhny Novgorod", 56.337287, 43.963251, "https://upload.wikimedia.org/wikipedia/commons/9/90/Nizhny_Novgorod_Stadium_%28March_2018%29.jpg");
+INSERT INTO stadiums (name, city, lat, lng, image) VALUES ("Cosmos Arena", "Samara", 53.278146, 50.238854, "http://img.fifa.com/image/upload/t_l4/d0mymt1ubl2pypmu3gn3.jpg");
+INSERT INTO stadiums (name, city, lat, lng, image) VALUES ("Volgograd Arena", "Volgograd", 48.734195, 44.548345, "https://upload.wikimedia.org/wikipedia/commons/6/61/Construction_of_Volgograd_Arena_inside_04.jpg");
+INSERT INTO stadiums (name, city, lat, lng, image) VALUES ("Mordovia Arena", "Saransk", 54.181795, 45.203851, "https://upload.wikimedia.org/wikipedia/commons/c/c9/%D0%A1%D1%82%D0%B0%D0%B4%D0%B8%D0%BE%D0%BD_Mordovia_arena.jpg");
+INSERT INTO stadiums (name, city, lat, lng, image) VALUES ("Rostov Arena", "Rostov-on-Don", 47.209581, 39.738424, "https://upload.wikimedia.org/wikipedia/commons/9/94/Rostov-Arens_%28april_2018%29_01.jpg");
+INSERT INTO stadiums (name, city, lat, lng, image) VALUES ("Fisht Olympic Stadium", "Sochi", 43.402011, 39.955709, "https://upload.wikimedia.org/wikipedia/commons/e/e6/Luzhniki_Stadium%2C_Moscow.jpg");
+INSERT INTO stadiums (name, city, lat, lng, image) VALUES ("Central Stadium", "Yekaterinburg", 56.83249, 60.573585, "https://upload.wikimedia.org/wikipedia/commons/d/d2/Estadio_Central_%28Ekaterinburg-arena%29.jpg");
 
-INSERT INTO brackets (group, date, stadium, channels, matchday) VALUES ("group", "date", "stadium", "channels", "matchday");
+

a24f01c5aa8bed6826fa6c78c921aebf511e73cf
m0bius <ezra.cook@u.northwestern.edu>
2018-06-06 13:33:07 -0500

    merge again

---
+++ seeds.sql
@@ -0,0 +1,38 @@
+USE world_cup;
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Russia", "flag-ru", "🇷🇺");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Saudi Arabia", "flag-sa", "🇸🇦");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Egypt", "flag-eg", "🇪🇬");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Uruguay", "flag-uy", "🇺🇾");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Portugal", "flag-pt", "🇵🇹");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Spain", "flag-es", "🇪🇸");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Morocco", "flag-ma", "🇲🇦");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Iran", "flag-ir", "🇮🇷");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("France", "flag-fr", "🇫🇷");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Australia", "flag-au", "🇦🇺");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Peru", "flag-pr", "🇵🇪");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Denmark", "flag-dk", "🇩🇰");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Argentina", "flag-ar", "🇦🇷");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Iceland", "flag-is", "🇮🇸");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Croatia", "flag-hr", "🇭🇷");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Nigeria", "flag-ng", "🇳🇬");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Brazil", "flag-br", "🇧🇷");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Switzerland", "flag-ch", "🇨🇭");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Costa Rica", "flag-cr", "🇨🇷");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Serbia", "flag-rs", "🇷🇸");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Germany", "flag-de", "🇩🇪");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Mexico", "flag-mx", "🇲🇽");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Sweden", "flag-se", "🇸🇪");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("South Korea", "flag-kr", "🇰🇷");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Belgium", "flag-be", "🇧🇪");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Panama", "flag-pa", "🇵🇦");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Tunisia", "flag-tn", "🇹🇳");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("England", "flag-england", "🏴󠁧󠁢󠁥󠁮󠁧󠁿");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Poland", "flag-pl", "🇵🇱");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Sengal", "flag-sn", "🇸🇳");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Colombiua", "flag-co", "🇨🇴");
+INSERT INTO teams (name, emoji, emojiString) VALUES ("Japan", "flag-jp", "🇯🇵");
+
+
+INSERT INTO stadiums (name, city, lat, lng, image) VALUES ("name", "city", "lat", "lng", "image");
+
+INSERT INTO brackets (group, date, stadium, channels, matchday) VALUES ("group", "date", "stadium", "channels", "matchday");
